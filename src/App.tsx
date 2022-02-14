6eb1600e3af0a333b27fc777f1fba55f233eb482
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2022-02-14 15:26:52 -0300

    :sparkles: Added react-router-dom
    - Added react-router-dom and fixed router file extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
-    <div className="App">
-        Hello
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
