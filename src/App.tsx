ed1c71810908040b8e052bad9083b56fd8084766
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2022-02-15 12:29:34 -0300

    :sparkles: Added Light Theme and Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
-import { AppRoutes } from "./routes";
+import { ThemeProvider } from '@mui/material';
+
+import { BrowserRouter } from 'react-router-dom';
+import { LightTheme } from './shared/themes';
+import { AppRoutes } from './routes';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
-}
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,18 @@
+import { Button } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router-dom';
-
 export const AppRoutes = () => {
-
   return (
     <Routes>
-      <Route path= "/pagina-inicial" element = {<p> Página inicial </p>} />
-          
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button variant="contained" color="primary">
+            Submit
+          </Button>
+        }
+      />
+
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );
-}
+};
