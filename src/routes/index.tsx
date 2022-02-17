57cbb5cb47b715e05633f74bd134df93594be7ec
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2022-02-16 21:07:02 -0300

    :sparkles: Added Theme
    - Added Dark Theme and Toggle Theme

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,17 @@
 import { Button } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAppThemeContext } from '../shared/contexts';
+
 export const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
+
   return (
     <Routes>
       <Route
         path="/pagina-inicial"
         element={
-          <Button variant="contained" color="primary">
-            Submit
+          <Button variant="contained" color="primary" onClick={toggleTheme}>
+            Toggle
           </Button>
         }
       />
---
+++ src/shared/contexts/ThemeContext.tsx
@@ -0,0 +1,49 @@
+import {
+  createContext,
+  useState,
+  useCallback,
+  useMemo,
+  useContext,
+} from 'react';
+import { ThemeProvider } from '@mui/material';
+import { LightTheme, DarkTheme } from '../themes';
+import { Box } from '@mui/system';
+
+interface IThemeContextData {
+  themeName: 'light' | 'dark';
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext({} as IThemeContextData);
+
+export const useAppThemeContext = () => {
+  return useContext(ThemeContext);
+};
+
+export const AppThemeProvider: React.FC = ({ children }) => {
+  const [themeName, setThemeName] = useState<'light' | 'dark'>('light');
+  const toggleTheme = useCallback(() => {
+    setThemeName((oldThemeName) =>
+      oldThemeName === 'light' ? 'dark' : 'light'
+    );
+  }, []);
+
+  const theme = useMemo(() => {
+    if (themeName === 'light') return LightTheme;
+    return DarkTheme;
+  }, [themeName]);
+
+  return (
+    <ThemeContext.Provider value={{ themeName, toggleTheme }}>
+      <ThemeProvider theme={theme}>
+        <Box
+          width="100vw"
+          height="100vh"
+          bgcolor={theme.palette.background.default}
+        >
+          {children}
+        </Box>
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
