2ed8c21dbb68106c63efaa7d3c106bf0bb6bb73b
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2022-02-25 09:06:53 -0300

    :sparkles: Setup Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,20 @@
 import { Button } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useDrawerContext } from '../shared/contexts';
+import { useEffect } from 'react';
 
 export const AppRoutes = () => {
-  const { toggleDrawerOpen } = useDrawerContext();
+  const { toggleDrawerOpen, setDrawerOptions } = useDrawerContext();
+
+  useEffect(() => {
+    setDrawerOptions([
+      {
+        icon: 'home',
+        path: '/pagina-inicial',
+        label: 'Página inicial',
+      },
+    ]);
+  }, []);
 
   return (
     <Routes>
@@ -15,7 +26,7 @@ export const AppRoutes = () => {
             color="primary"
             onClick={toggleDrawerOpen}
           >
-            Toggle
+            Menu
           </Button>
         }
       />
