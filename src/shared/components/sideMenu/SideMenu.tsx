00ea63a4599efcf4f115716cb13be1d509ea7de5
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2022-02-25 09:06:18 -0300

    :sparkles: Updated Menu
    - Setup menu options and context.

--- src/shared/components/sideMenu/SideMenu.tsx
+++ src/shared/components/sideMenu/SideMenu.tsx
@@ -11,12 +11,46 @@ import {
   useTheme,
 } from '@mui/material';
 import { Box } from '@mui/system';
+import { useNavigate, useResolvedPath, useMatch } from 'react-router-dom';
 import { useDrawerContext } from '../../contexts';
+
+interface IListItemLinkProps {
+  to: string;
+  label: string;
+  icon: string;
+  onClick: (() => void) | undefined;
+}
+
+const ListItemLink: React.FC<IListItemLinkProps> = ({
+  to,
+  icon,
+  label,
+  onClick,
+}) => {
+  const navigate = useNavigate();
+
+  const resolvedPath = useResolvedPath(to);
+  const match = useMatch({ path: resolvedPath.pathname, end: false });
+
+  const handleClick = () => {
+    navigate(to);
+    onClick?.();
+  };
+
+  return (
+    <ListItemButton selected={!!match} onClick={handleClick}>
+      <ListItemIcon>
+        <Icon>{icon}</Icon>
+      </ListItemIcon>
+      <ListItemText primary={label} />
+    </ListItemButton>
+  );
+};
 
 export const SideMenu: React.FC = ({ children }) => {
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
-  const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
+  const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext();
 
   const username = 'SilasRodrigues19';
 
@@ -54,12 +88,15 @@ export const SideMenu: React.FC = ({ children }) => {
           <Divider />
           <Box flex={1}>
             <List component="nav">
-              <ListItemButton>
-                <ListItemIcon>
-                  <Icon>home</Icon>
-                </ListItemIcon>
-                <ListItemText primary="Página Inicial" />
-              </ListItemButton>
+              {drawerOptions.map(({ path, icon, label }) => (
+                <ListItemLink
+                  to={path}
+                  key={path}
+                  icon={icon}
+                  label={label}
+                  onClick={smDown ? toggleDrawerOpen : undefined}
+                />
+              ))}
             </List>
           </Box>
         </Box>
--- src/shared/contexts/DrawerContext.tsx
+++ src/shared/contexts/DrawerContext.tsx
@@ -1,8 +1,16 @@
 import { createContext, useCallback, useContext, useState } from 'react';
 
+interface IDrawerOptions {
+  icon: string;
+  path: string;
+  label: string;
+}
+
 interface IDrawerContextData {
   isDrawerOpen: boolean;
   toggleDrawerOpen: () => void;
+  drawerOptions: IDrawerOptions[];
+  setDrawerOptions: (newDrawerOptions: IDrawerOptions[]) => void;
 }
 
 const DrawerContext = createContext({} as IDrawerContextData);
@@ -13,13 +21,28 @@ export const useDrawerContext = () => {
 
 export const DrawerProvider: React.FC = ({ children }) => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [drawerOptions, setDrawerOptions] = useState<IDrawerOptions[]>([]);
 
   const toggleDrawerOpen = useCallback(() => {
     setIsDrawerOpen((oldDrawerOpen) => !oldDrawerOpen);
   }, []);
 
+  const handleSetDrawerOptions = useCallback(
+    (newDrawerOptions: IDrawerOptions[]) => {
+      setDrawerOptions(newDrawerOptions);
+    },
+    []
+  );
+
   return (
-    <DrawerContext.Provider value={{ isDrawerOpen, toggleDrawerOpen }}>
+    <DrawerContext.Provider
+      value={{
+        isDrawerOpen,
+        drawerOptions,
+        toggleDrawerOpen,
+        setDrawerOptions: handleSetDrawerOptions,
+      }}
+    >
       {children}
     </DrawerContext.Provider>
   );
