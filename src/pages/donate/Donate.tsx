f0a192abc8a4e0fbe7619fcb158865d9173fb9d0
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2022-04-15 14:12:36 -0300

    feat: add donate page card

--- src/pages/donate/Donate.tsx
+++ src/pages/donate/Donate.tsx
@@ -13,13 +13,40 @@ import {
   useTheme,
 } from '@mui/material';
 
+import React from 'react';
+import { styled } from '@mui/material/styles';
+import Collapse from '@mui/material/Collapse';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 import bg from '../../images/donate.svg';
 
-export const Donate = () => {
+export const Donate: React.FC = () => {
   const theme = useTheme();
 
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  interface ExpandMoreProps extends IconButtonProps {
+    expand: boolean;
+  }
+
+  const ExpandMore = styled((props: ExpandMoreProps) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
+
   return (
-    <BaseLayoutOfPages title="Contributions ">
+    <BaseLayoutOfPages title="Contributions">
       <Divider variant="middle" sx={{ mx: 2 }} />
       <Card
         sx={{ width: '75%', margin: '30px auto' }}
@@ -28,7 +55,7 @@ export const Donate = () => {
       >
         <CardHeader
           title="Support us"
-          sx={{ textAlign: 'center', letterSpacing: '2em !important' }}
+          sx={{ textAlign: 'center' }}
         ></CardHeader>
         <Divider variant="middle" sx={{ margin: '10px 0' }} />
         <CardMedia
@@ -78,7 +105,63 @@ export const Donate = () => {
               Share
             </Typography>
           </Button>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
         </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph fontWeight="600">
+              How contributions help us:
+            </Typography>
+            <Typography paragraph color="text.secondary">
+              Any and all donations will go to the project, with the goal of
+              making it a scalable system that can reach a larger number of
+              people.
+            </Typography>
+            <Typography paragraph color="text.secondary">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. In a
+              minima veritatis, doloremque commodi praesentium nesciunt? Magnam
+              tenetur assumenda sit, ullam earum quam minus similique iste, aut,
+              perspiciatis reiciendis laudantium! Nobis aut porro harum,
+              aspernatur, sequi ducimus perspiciatis ipsum nisi provident
+              corrupti quas modi. Voluptatum saepe deserunt temporibus,
+              architecto ullam numquam unde dolores totam non ad. Ab culpa
+              facilis distinctio. Delectus, earum culpa esse quasi enim animi
+              eaque tempore id quidem est velit quam? Sint at debitis, itaque
+              numquam exercitationem ducimus alias dignissimos autem labore
+              aperiam voluptatum, vel suscipit hic? Possimus consequatur dolorum
+              rem tenetur tempore sint dolores commodi libero aperiam temporibus
+              ex iure error eaque minima qui aliquam ullam, debitis, fuga et
+              perferendis! Consequatur illum mollitia perferendis sequi
+              quisquam.
+            </Typography>
+            <Typography paragraph color="text.secondary">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+              minima inventore error dolore cupiditate assumenda porro ipsa
+              exercitationem repellat vel nostrum quod suscipit officiis, nemo
+              corrupti totam explicabo aspernatur recusandae? Dignissimos qui
+              tempore nam deserunt minima excepturi. Obcaecati ex veniam nisi
+              architecto cumque exercitationem, beatae optio debitis quo
+              corporis rerum. Perspiciatis quae facilis consequuntur labore
+              obcaecati harum iure officiis quam. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Hic minima quia labore ullam
+              doloribus fugiat nam ipsam modi. Sint velit aspernatur eligendi
+              cum ex tenetur totam exercitationem commodi quam mollitia.
+            </Typography>
+            <Typography color="text.secondary">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+              quis ipsam voluptatum eum veritatis? Sunt minus amet earum quasi
+              inventore, laboriosam qui unde non, sit nostrum quia ipsum quidem
+              dolore!
+            </Typography>
+          </CardContent>
+        </Collapse>
       </Card>
     </BaseLayoutOfPages>
   );
