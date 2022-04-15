ef25b06432af8ea2aef20f3644a179f5fb35710c
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2022-04-15 14:12:55 -0300

    feat: add help page card

--- src/pages/help/Help.tsx
+++ src/pages/help/Help.tsx
@@ -1,10 +1,42 @@
 import { BaseLayoutOfPages } from '../../shared/layouts';
-import { Divider } from '@mui/material';
+import {
+  Divider,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  Typography,
+  Paper,
+} from '@mui/material';
 
-export const Help = () => {
+import React from 'react';
+
+import bg from '../../images/faq.svg';
+
+export const Help: React.FC = () => {
   return (
     <BaseLayoutOfPages title="Help">
       <Divider variant="middle" sx={{ mx: 2 }} />
+      <Card
+        sx={{ width: '75%', margin: '30px auto' }}
+        component={Paper}
+        variant="outlined"
+      >
+        <CardHeader title="FAQ" sx={{ textAlign: 'center' }}></CardHeader>
+        <Divider variant="middle" sx={{ margin: '10px 0' }} />
+        <CardMedia
+          sx={{ objectFit: 'contain' }}
+          component="img"
+          height="194"
+          image={bg}
+          alt="Paella dish"
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            Help page.
+          </Typography>
+        </CardContent>
+      </Card>
     </BaseLayoutOfPages>
   );
 };
