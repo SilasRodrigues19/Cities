74bf0c81092b3c2736bc5fcd36c42dfb7c1a4314
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2022-05-03 20:26:34 -0300

    refactor: add tabs component

--- src/pages/info/Info.tsx
+++ src/pages/info/Info.tsx
@@ -10,18 +10,53 @@ import {
   useTheme,
 } from '@mui/material';
 
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+
 import React from 'react';
 import bg from '../../images/about_us.svg';
 
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import { TipsAndUpdates, AutoGraph, RocketLaunch } from '@mui/icons-material';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 export const Info: React.FC = () => {
   const theme = useTheme();
-  const [value, setValue] = React.useState('recents');
+  const [value, setValue] = React.useState(0);
 
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
@@ -50,31 +85,53 @@ export const Info: React.FC = () => {
             Info page.
           </Typography>
         </CardContent>
-        <BottomNavigation
-          sx={{
-            width: 500,
-            margin: '60px auto',
-            background: theme.palette.mode === 'dark' ? 'transparent' : '',
-          }}
-          value={value}
-          onChange={handleChange}
-        >
-          <BottomNavigationAction
-            label="Mission"
-            value="mission"
-            icon={<RocketLaunch />}
-          />
-          <BottomNavigationAction
-            label="Vison"
-            value="vision"
-            icon={<TipsAndUpdates />}
-          />
-          <BottomNavigationAction
-            label="Values"
-            value="values"
-            icon={<AutoGraph />}
-          />
-        </BottomNavigation>
+        <Box sx={{ width: '100%' }}>
+          <Box
+            sx={{
+              borderBottom: 1,
+              borderColor: 'divider',
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="organization tabs"
+              sx={{ gap: '3rem' }}
+            >
+              <Tab
+                sx={{
+                  textTransform: 'capitalize',
+                }}
+                icon={<RocketLaunch />}
+                label="Mission"
+                {...a11yProps(0)}
+              />
+              <Tab
+                sx={{ textTransform: 'capitalize' }}
+                icon={<TipsAndUpdates />}
+                label="Vision"
+                {...a11yProps(1)}
+              />
+              <Tab
+                sx={{ textTransform: 'capitalize' }}
+                icon={<AutoGraph />}
+                label="Values"
+                {...a11yProps(2)}
+              />
+            </Tabs>
+          </Box>
+          <TabPanel value={value} index={0}>
+            Mission
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            Vision
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            Values
+          </TabPanel>
+        </Box>
       </Card>
     </BaseLayoutOfPages>
   );
