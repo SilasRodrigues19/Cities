8bf9d162656609f0324648c81a7ac603914e0d31
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-06-04 23:04:43 -0300

    feat: add cards for total cities and people

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import { BaseLayout } from '../../shared/layouts';
 import { DetailTools } from '../../shared/components';
 
-import { useTheme, Box, SpeedDial, SpeedDialIcon, SpeedDialAction } from '@mui/material';
-import { Info, Help, Apartment, SupervisorAccount, VolunteerActivism } from '@mui/icons-material';
+import { useTheme, Box, SpeedDial, SpeedDialIcon, SpeedDialAction, Grid, Card, CardContent, Typography } from '@mui/material';
+import { Info, Help, Apartment, SupervisorAccount, VolunteerActivism, StrikethroughSTwoTone } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
-
+import { useEffect, useState } from 'react';
+import { CitiesService } from '../../shared/services/api/cities/CitiesService';
+import { PeopleService } from '../../shared/services/api/people/PeopleService';
 
 const navigateTo: any = (to: string, children: string) => (
   <Link to={to}>{children}</Link>
@@ -21,13 +23,101 @@ const actions = [
 export const Dashboard = () => {
   const theme = useTheme();
 
+  const [isCitiesloading, setCitiesIsLoading] = useState(true);
+  const [isPeopleloading, setPeopleIsLoading] = useState(true);
+
+  const [totalCitiesCount, setTotalCitiesCount] = useState(0);
+  const [totalPeopleCount, setTotalPeopleCount] = useState(0);
+
+  useEffect(() => {
+    setCitiesIsLoading(true)
+    setPeopleIsLoading(true)
+
+    CitiesService.getAll(1)
+      .then((result) => {
+        setCitiesIsLoading(false);
+
+        if (result instanceof Error) {
+          alert(result.message);
+          return;
+        }
+        setTotalCitiesCount(result.totalCount);
+      });
+    PeopleService.getAll(1)
+      .then((result) => {
+        setPeopleIsLoading(false);
+
+        if (result instanceof Error) {
+          alert(result.message);
+          return;
+        }
+        setTotalPeopleCount(result.totalCount);
+      });
+  }, [])
+
   return (
     <BaseLayout
       title="Dashboard"
       toolbar={
-        <DetailTools showNewButton showSaveCloseButton showBackButton={false} />
+        <DetailTools showNewButton={false} showDeleteButton={false} showSaveButton={false} showSaveCloseButton={false} showBackButton={false} />
       }
     >
+      <Box width='100%' display='flex'>
+        <Grid container margin={2}>
+          <Grid item container spacing={2}>
+
+            <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+
+              <Card>
+                <CardContent>
+                  <Typography variant='h5' align='center'>
+                    Total People
+                  </Typography>
+
+                  <Box padding={6} display='flex' justifyContent='center' alignItems='center'>
+                    {!isPeopleloading && (
+                      <Typography variant='h1'>
+                        {totalPeopleCount}
+                      </Typography>
+                    )}
+                    {isPeopleloading && (
+                      <Typography variant='h6'>
+                        Carregando...
+                      </Typography>
+                    )}
+                  </Box>
+                </CardContent>
+              </Card>
+
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+
+              <Card>
+                <CardContent>
+                  <Typography variant='h5' align='center'>
+                    Total Cities
+                  </Typography>
+
+                  <Box padding={6} display='flex' justifyContent='center' alignItems='center'>
+                    {!isCitiesloading && (
+                      <Typography variant='h1'>
+                        {totalCitiesCount}
+                      </Typography>
+                    )}
+                    {isCitiesloading && (
+                      <Typography variant='h6'>
+                        Carregando...
+                      </Typography>
+                    )}
+                  </Box>
+                </CardContent>
+              </Card>
+
+            </Grid>
+
+          </Grid>
+        </Grid>
+      </Box>
       <Box
         sx={{
           position: 'fixed',
