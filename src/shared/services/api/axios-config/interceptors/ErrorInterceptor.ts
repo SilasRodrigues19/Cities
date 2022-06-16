9f66670e622a216dc685353dc099f0c41a2787fb
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-06-16 14:22:45 -0300

    refactor: update axios error message

--- src/shared/services/api/axios-config/interceptors/ErrorInterceptor.ts
+++ src/shared/services/api/axios-config/interceptors/ErrorInterceptor.ts
@@ -2,7 +2,7 @@ import { AxiosError } from 'axios';
 
 export const errorInterceptor = (error: AxiosError) => {
   if (error.message === 'Network Error') {
-    return Promise.reject(new Error('Network Error.'));
+    return Promise.reject(new Error('Data mock could not be found.'));
   }
 
   if (error.response?.status === 401) {
